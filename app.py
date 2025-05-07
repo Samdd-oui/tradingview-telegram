c9518c1e2725bad15f5e598d413067c6e3e4c2ca
Samy <samydd99@gmail.com>
2025-05-07 16:59:54 +0200

    debug: print raw data

--- app.py
+++ app.py
@@ -1,20 +1,13 @@
-from flask import Flask, request
-import requests
-import json
-
-app = Flask(__name__)
-
-BOT_TOKEN = "7681427635:AAHLK2hvr2ooTiQJV_axXeGRlTffh7-HTKg"
-CHAT_ID = "-1002501325876"
-
 @app.route("/", methods=["POST"])
 def alert():
     try:
-        # Accepte JSON ou texte brut
+        raw_data = request.data.decode("utf-8")
+        print("RAW DATA:", raw_data)
+
         if request.is_json:
             data = request.get_json()
         else:
-            data = json.loads(request.data.decode("utf-8"))
+            data = json.loads(raw_data)
 
         message = data.get("message", "⚠️ Alerte reçue, mais sans message clair.")
 
